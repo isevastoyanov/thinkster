9d40d0bea35be922cb785e06825d6c41756ed2de
Ivan Sevastoyanov <ivan.sevastoyanov@gmail.com>
2016-07-24 22:48:36 +0300

    Create a login.html template and other stuff

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,13 @@
-from rest_framework import permissions, response, status, viewsets
+import json
 
 from authentication.models import Account
 from authentication.permissions import IsAccountOwner
 from authentication.serializers import AccountSerializer
+
+from django.contrib.auth import authenticate, login
+
+from rest_framework import permissions, response, status, views, viewsets
+from rest_framework.response import Response
 
 
 class AccountViewSet(viewsets.ModelViewSet):
@@ -31,3 +36,31 @@ class AccountViewSet(viewsets.ModelViewSet):
             'status': 'Bad request',
             'message': 'Account could not be created with received data.'
         }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled.'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username/password combination invalid.'
+            }, status=status.HTTP_401_UNAUTHORIZED)
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,4 +1,4 @@
-from authentication.views import AccountViewSet
+from authentication.views import AccountViewSet, LoginView
 from django.conf.urls import include, patterns, url
 from rest_framework_nested import routers
 from thinkster_django_angular_boilerplate.views import IndexView
@@ -9,5 +9,6 @@ router.register(r'accounts', AccountViewSet)
 urlpatterns = patterns(
     '',
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url('^.*$', IndexView.as_view(), name='index'),
 )
